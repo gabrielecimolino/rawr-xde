0bc56d6f97d508b8009a437f2e105e7432139b84
gabrielecimolino <gabrielecimolino@gmail.com>
2018-03-22 11:04:04 -0400

    Added search

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, TextInput, Dimensions, Image, ImageBackground, BlurView, Button, FlatList } from 'react-native';
+import { Text, View, TextInput, Dimensions, Image, ImageBackground, BlurView, Button, FlatList, Alert } from 'react-native';
 import { Asset, Font, AppLoading } from 'expo';
 
 
@@ -57,6 +57,11 @@ class AppIcon extends React.Component{
     }
   }
 
+  contains(searchString){
+    Alert.alert('Contains');
+    return this.props.name.includes(searchString);
+  }
+
   render(){
     if(this.state.selected){
       return(
@@ -111,10 +116,10 @@ class AppIcon extends React.Component{
   }
 }
 
-export default class App extends React.Component {
+export default class App extends React.Component {  
   constructor(props){
     super(props);
-    this.state = { fontLoaded: false };
+    this.state = { fontLoaded: false, workingSet: [], searchString: '', init: false };
   }
 
   async componentDidMount() {
@@ -122,7 +127,27 @@ export default class App extends React.Component {
       'sf-pro': require('./assets/fonts/SFProText-Regular.otf'),
     });
 
-    this.setState({ fontLoaded: true });
+    this.setState({ fontLoaded: true, appIcons: [
+        <AppIcon name= { 'App Store' } source={ require('./assets/images/icons/appstore.png') } key={ 'App Store' } />,
+        <AppIcon name= { 'Calendar' } source={ require('./assets/images/icons/calendar.png') } key={ 'Calendar' } />,
+        <AppIcon name= { 'Facebook' } source={ require('./assets/images/icons/fb.png') } key={ 'Facebook' } />
+      ] });
+    this.setState({ workingSet: this.state.appIcons });
+  }
+
+  contains(key, searchString){
+    return key.toLowerCase().indexOf(searchString.toLowerCase()) !== -1;
+  }
+
+  filterApps(searchString, icons){
+    //Alert.alert(searchString);
+    //this.state.workingSet.map((item) => Alert.alert(item.key));
+    this.setState({ searchString: searchString, workingSet: icons.filter((item) => this.contains(item.key, searchString)) });
+  }
+
+  init(){
+    Alert.alert('init');
+    this.setState({ workingSet: appIcons, init: true });
   }
 
   render() {
@@ -131,11 +156,15 @@ export default class App extends React.Component {
       return (<AppLoading />);
     }
     else{
-      const appIcons = [
-        <AppIcon name= { 'App Store' } source={ require('./assets/images/icons/appstore.png') } key={ ' 1 ' } />,
-        <AppIcon name= { 'Calendar' } source={ require('./assets/images/icons/calendar.png') } key={ ' 2 ' } />,
-        <AppIcon name= { 'Facebook' } source={ require('./assets/images/icons/fb.png') } key={ ' 3 ' } />
-      ];
+      // const appIcons = [
+      //   <AppIcon name= { 'App Store' } source={ require('./assets/images/icons/appstore.png') } key={ ' 1 ' } />,
+      //   <AppIcon name= { 'Calendar' } source={ require('./assets/images/icons/calendar.png') } key={ ' 2 ' } />,
+      //   <AppIcon name= { 'Facebook' } source={ require('./assets/images/icons/fb.png') } key={ ' 3 ' } />
+      // ];
+
+      // if(!this.state.init){
+      //   this.init();
+      // }
 
       return (
         <View tint="light" intensity={ 50 } style={{ flex: 1 }}>
@@ -150,7 +179,7 @@ export default class App extends React.Component {
                       style={{ color: '#fff', fontFamily: 'sf-pro', marginLeft: Dimensions.get('window').width / 10, width: Dimensions.get('window').width / 1.5}}
                       placeholder="Search"
                       textInputProps={{underlineColorAndroid:'rgba(0,0,0,0)'}}
-                      onChangeText={(text) => this.setState({text})}
+                      onChangeText={ (text) => { this.setState({text}); this.filterApps(text, this.state.appIcons); } }  
                     />
                     <DoneButton/>
                   </View>
@@ -163,7 +192,7 @@ export default class App extends React.Component {
                   
                   <ImageBackground style={{ height: Dimensions.get('window').height / 2, width: Dimensions.get('window').width * 0.8, flexDirection: 'row', flexWrap: 'wrap' }} source={require('./assets/images/folder.png')} >
                     
-                    { appIcons }
+                      { this.state.workingSet }
                 
                   </ImageBackground>
 
