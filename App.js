f2ad47db50393920125b4bd2f1c25ed4c02ee73f
gabrielecimolino <gabrielecimolino@gmail.com>
2018-03-21 23:50:15 -0400

    Added icons

--- App.js
+++ App.js
@@ -1,6 +1,115 @@
 import React from 'react';
-import { Text, View, TextInput, Dimensions, Image, ImageBackground, BlurView } from 'react-native';
+import { Text, View, TextInput, Dimensions, Image, ImageBackground, BlurView, Button, FlatList } from 'react-native';
 import { Asset, Font, AppLoading } from 'expo';
+
+
+class DoneButton extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = { pressed: false };
+  }
+  
+  press(){
+    if (this.state.pressed) {
+      this.setState({ pressed: false });
+    } else {
+      this.setState({ pressed: true });
+    }
+  }
+  
+  render(){
+    if(this.state.pressed){
+      return (
+        <View style= {{ width: Dimensions.get('window').width / 5 }}>
+          <Button
+            onPress={ this.press.bind(this) }
+            title= 'Done'
+            color= '#000'
+          />
+        </View>
+      );
+    }
+    else{
+      return (
+        <View style= {{ width: Dimensions.get('window').width / 5 }}>
+          <Button
+            onPress={ this.press.bind(this) }
+            title= 'Done'
+            color= '#fff'
+          />
+        </View>
+      );
+    }
+  }
+}
+
+class AppIcon extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = { selected: false };
+  }
+
+  press(){
+    if (this.state.selected) {
+      this.setState({ selected: false });
+    } else {
+      this.setState({ selected: true });
+    }
+  }
+
+  render(){
+    if(this.state.selected){
+      return(
+        <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center', height: Dimensions.get('window').height / 6, width: Dimensions.get('window').width * 0.8 / 3 }}>
+          <View style={{ }}>
+            <ImageBackground style={{ flex: 1, flexDirection: 'row', justifyContent: 'center', height: Dimensions.get('window').height * 0.7 / 6, width: Dimensions.get('window').width * 0.7 * 0.8 / 3 }} source={this.props.source} >
+
+              <View style={{  }}>
+
+                <ImageBackground source= { require('./assets/images/icons/check.png') } style={{ flex: 1, flexDirection: 'row', justifyContent: 'center', height: Dimensions.get('window').height * 0.7 / 6, width: Dimensions.get('window').width * 0.7 * 0.8 / 3 }} >
+
+                  <Button 
+                    onPress={ this.press.bind(this) }
+                    title= '                                                                                          '
+                    color= 'rgba(0,0,0,0)'
+                    style={{ width: Dimensions.get('window').width  }}
+                  >
+                  </Button>
+
+                </ImageBackground>
+
+              </View>
+
+            </ImageBackground>
+
+            <Text style={{ color: '#fff', fontFamily: 'sf-pro', textAlign: 'center' }}>{this.props.name}</Text>
+
+          </View>  
+        </View>
+      );
+    }
+    else{
+      return(
+        <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center', height: Dimensions.get('window').height / 6, width: Dimensions.get('window').width * 0.8 / 3 }} >
+          
+          <View style={{  }}>
+            <ImageBackground style={{ flex: 1, flexDirection: 'row', justifyContent: 'center', height: Dimensions.get('window').height * 0.7 / 6, width: Dimensions.get('window').width * 0.7 * 0.8 / 3 }} source={this.props.source} >
+              <Button 
+                onPress={ this.press.bind(this) }
+                title= '                                                                                    '
+                color= 'rgba(0,0,0,0)'
+                style={{ width: Dimensions.get('window').width  }}
+              />
+            </ImageBackground>
+
+            <Text style={{ color: '#fff', fontFamily: 'sf-pro', textAlign: 'center' }}>{this.props.name}</Text>
+          </View>  
+
+        </View>
+      );
+    }
+  }
+}
 
 export default class App extends React.Component {
   constructor(props){
@@ -17,10 +126,17 @@ export default class App extends React.Component {
   }
 
   render() {
+
     if(!this.state.fontLoaded){
       return (<AppLoading />);
     }
     else{
+      const appIcons = [
+        <AppIcon name= { 'App Store' } source={ require('./assets/images/icons/appstore.png') } key={ ' 1 ' } />,
+        <AppIcon name= { 'Calendar' } source={ require('./assets/images/icons/calendar.png') } key={ ' 2 ' } />,
+        <AppIcon name= { 'Facebook' } source={ require('./assets/images/icons/fb.png') } key={ ' 3 ' } />
+      ];
+
       return (
         <View tint="light" intensity={ 50 } style={{ flex: 1 }}>
 
@@ -28,21 +144,29 @@ export default class App extends React.Component {
               
               <View style={{ marginTop: Dimensions.get('window').height / 25, height: Dimensions.get('window').height / 15 }}>
                 <ImageBackground style={{ height: Dimensions.get('window').height / 15, width: Dimensions.get('window').width}} source={require('./assets/images/searchbar.png')} >
-                  <TextInput
-                    style={{ color: '#fff', flex: 1, fontFamily: 'sf-pro', marginLeft: Dimensions.get('window').width / 10 }}
-                    placeholder="Search"
-                    textInputProps={{underlineColorAndroid:'rgba(0,0,0,0)'}}
-                    onChangeText={(text) => this.setState({text})}
-                  />
+                  
+                  <View style={{ flex:1, flexDirection: 'row' }}>
+                    <TextInput
+                      style={{ color: '#fff', fontFamily: 'sf-pro', marginLeft: Dimensions.get('window').width / 10, width: Dimensions.get('window').width / 1.5}}
+                      placeholder="Search"
+                      textInputProps={{underlineColorAndroid:'rgba(0,0,0,0)'}}
+                      onChangeText={(text) => this.setState({text})}
+                    />
+                    <DoneButton/>
+                  </View>
+
                 </ImageBackground>
               </View>
 
               <View style={{ alignItems: 'center' }}>
                 <View style={{ marginTop: Dimensions.get('window').height / 8, height: Dimensions.get('window').height / 2.5, width: Dimensions.get('window').width * 0.8, alignItems: 'center' }}>
                   
-                  <ImageBackground style={{ height: Dimensions.get('window').height / 2, width: Dimensions.get('window').width * 0.8 }} source={require('./assets/images/folder.png')} >
-                    <Text style={{ textAlign: 'center', textAlignVertical: 'center', fontFamily: 'sf-pro', fontSize: Dimensions.get('window').height / 10 }}>Grid</Text>
+                  <ImageBackground style={{ height: Dimensions.get('window').height / 2, width: Dimensions.get('window').width * 0.8, flexDirection: 'row', flexWrap: 'wrap' }} source={require('./assets/images/folder.png')} >
+                    
+                    { appIcons }
+                
                   </ImageBackground>
+
                 </View>
               </View>
 
@@ -53,98 +177,3 @@ export default class App extends React.Component {
     }
   }
 }
-
-
-// import React, { Component } from 'react';
-// import { AppRegistry, Text, TextInput, View, StyleSheet } from 'react-native';
-// import { Font } from 'expo';
-
-// export default class Fixed extends Component {
-//   async componentDidMount() {
-//     await Font.loadAsync({
-//       'SF-Pro': require('./assets/fonts/SFProText-Regular.otf'),
-//     });
-
-//     this.setState({ fontLoaded: true });
-//   }
-
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <View style={styles.tbar}>
-//           <Text style={styles.text}>Fixed top bar</Text>
-//         </View>
-//         <ScrollView style={styles.main}>
-//           <View style={styles.item}><Text style={styles.text}>1</Text></View>
-//           <View style={styles.item}><Text style={styles.text}>2</Text></View>
-//           <View style={styles.item}><Text style={styles.text}>3</Text></View>
-//         </ScrollView>
-//         <View style={styles.bbar}>
-//           <Text style={styles.text}>Fixed bottom bar</Text>
-//         </View>
-//       </View>
-//     );
-//   }
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1
-//   },
-//   tbar: {
-//     width: 375,
-//     height: 100,
-//     borderBottomWidth: 5,
-//     borderColor: 'black',
-//     backgroundColor: 'red'
-//   },
-//   main: {
-//     flex: 1
-//   },
-//   item: {
-//     height: 200,
-//     width: 375,
-//     marginTop: 10,
-//     backgroundColor: 'green'
-//   },
-//   bbar: {
-//     width: 375,
-//     height: 100,
-//     borderTopWidth: 5,
-//     borderColor: 'black',
-//     backgroundColor: 'red'
-//   },
-//   text: {
-//     color: '#ffffff',
-//     fontFamily: 'SF-Pro',
-//     fontSize: 40
-//   }
-// });
-
-// import React, { Component } from 'react';
-// import { AppRegistry, Text, TextInput, View } from 'react-native';
-
-// export default class PizzaTranslator extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {text: ''};
-//   }
-
-//   render() {
-//     return (
-//       <View style={{padding: 10}}>
-//         <TextInput
-//           style={{flex: 1}}
-//           placeholder="Type here to ;____;!"
-//           onChangeText={(text) => this.setState({text})}
-//         />
-//         <Text style={{flex: 2, fontSize: 42}}>
-//           {this.state.text.split(' ').map((word) => word && ';_____;').join(' ')}
-//         </Text>
-//       </View>
-//     );
-//   }
-// }
-
-// skip this line if using Create React Native App
-//AppRegistry.registerComponent('AwesomeProject', () => PizzaTranslator);
